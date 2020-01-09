64d899f0166cbeb5d2a90b67da1da1c95f85d1ff
Ellen Park <ellenpark781@gmail.com>
2020-01-09 10:17:45 -0500

    created my files and created skeletal html and pseudo coded my app

---
+++ app.js
@@ -0,0 +1,12 @@
+​  // Allow user to "View Highscore" & "Time" value set at 0 
+  // Call to action: "Start Quiz"
+  function gameStart(){
+    console.log("the game has begun")
+    console.log(questions)
+}
+// Make a timer that starts at 75 minutes and goes down to 0 
+// For correct answers increase the score by 1
+// For any incorrect answers notify player and subtract 15 seconds 
+// At the end of the game, display final score 
+// prompt user to input their initials 
+// store the scores and initials in localStorage 
