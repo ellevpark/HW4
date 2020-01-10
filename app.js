0e70afafc61d7f0659cfef1cc30da512da116278
Ellen Park <ellenpark781@gmail.com>
2020-01-10 15:34:27 -0500

    created a score box

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
+// name DOM variables 
+
 var timeEl = document.querySelector(".time");
 var secondsLeft = 75;
 var index = 0;
 var score = 0;
 var timerInterval;
+var scoreBox = document.getElementById (scoreBox); 
 
-
+// Create a function that starts game 
 function gameStart(){
+// After the game starts, so will the timer, and the question display 
     setTime();
     displayQuestion();
 }
-
+// create a function that will take the timer that will decrement by 1 second 
 function setTime() {
     timerInterval = setInterval(function(){
         
         if(secondsLeft < 1) {
             clearInterval(timerInterval);
             console.log("time ended")
-            //call game over function
-            sendMessage ();
+            // TODO: call game over function
         }
-
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left."
     }, 1000)
 }
 
-//function sendMessage() {
-  //Show score and game over
+// TODO: function sendMessage() {
+  // TODO: Show score and game over
 //}
-
+// create a function to display questions one at a time 
 function displayQuestion (){
-    //clear out qbox
-    var title = questions[index].title;
-    console.log(title);
+// clear out qbox
     var qBox = document.querySelector("#questionBox");
+    qBox.textContent = "";
+    var title = questions[index].title;
 
     var titleEl = document.createElement("h2");
     titleEl.textContent = title;
-    console.log(titleEl);
     qBox.appendChild(titleEl);
 
     var choices = questions[index].choices;
@@ -46,17 +47,21 @@ function displayQuestion (){
         button.textContent = choice;
         button.addEventListener("click", checkAnswer)
         qBox.appendChild(button);
-    })
+      })
+      
 }
-
+// create a function that will check the answr to see if it is correct or incorrect
 function checkAnswer(event){
     //console.log(event.target.textContent)
     if(questions[index].answer === event.target.textContent){
         score++;
         alert("Correct!")
-        console.log (score)
+    var scoreBox= document.getElementById("scoreBox");
+    console.log 
+    scoreBox.innerHTML= "Score: " + score;
+        
     } else {
-        //secondsLeft = secondsLeft - 15;
+        //aka:secondsLeft = secondsLeft - 15;
         secondsLeft -= 15;
         alert("Wrong. -15 seconds")
     }
@@ -65,7 +70,15 @@ function checkAnswer(event){
     // function gameover displaying score, stop timer
     displayQuestion()
 }
+// TODO: function storeScores() {
+//   var highScore = localStorage.getItem("score");
 
+//   if (score === null) {
+//     return;
+//   }
+
+// }
+//I DONT THINK THIS WORKS..
 document.getElementById("start").addEventListener("click", gameStart)
 
 //Allow user to "View Highscore" & "Time" value set at 0
