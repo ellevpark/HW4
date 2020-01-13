5c39bd8040d1c68c7721d833e2efe0051bf2a255
Ellen Park <ellenpark781@gmail.com>
2020-01-13 15:38:24 -0500

    displayed whether the answer is correct or incorrect

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// TODO: 
+// Have end of game score = to the # of seconds left 
+// Ask the user for their initials 
+// Store the score and the user's initials in the local storage 
+// localStorage.setItem ("score"); 
+// TODO: Go back button bring you back to landing page/resets the game
+
+// document.getElementById ("result").innerHTMl = localStorage.getItem("score")
+// Be able to retrieve the highscores when the Highscore button is clicked 
+
 // name DOM variables 
 
 var timeEl = document.querySelector(".time");
-var secondsLeft = 75;
+var secondsLeft = 30;
 var index = 0;
 var score = 0;
 var timerInterval;
@@ -11,63 +21,58 @@ var scoreBox = document.getElementById (scoreBox);
 function gameStart(){
 // After the game starts, so will the timer, and the question display 
     setTime();
-    displayQuestion();
+        displayQuestion(); 
 }
 // create a function that will take the timer that will decrement by 1 second 
 function setTime() {
     timerInterval = setInterval(function(){
-        
-        if(secondsLeft < 1) {
-            clearInterval(timerInterval);
-            console.log("time ended")
-            // TODO: call game over function
-        }
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left."
+        if(secondsLeft < 1) { 
+            clearInterval(timerInterval);
+            secondsLeft=0;
+            
+        }
     }, 1000)
 }
-
-// TODO: function sendMessage() {
-  // TODO: Show score and game over
-//}
 // create a function to display questions one at a time 
 function displayQuestion (){
-// clear out qbox
-    var qBox = document.querySelector("#questionBox");
-    qBox.textContent = "";
-    var title = questions[index].title;
+    if(!questions[index]) {
+        displayGameOver()
 
-    var titleEl = document.createElement("h2");
-    titleEl.textContent = title;
-    qBox.appendChild(titleEl);
-
-    var choices = questions[index].choices;
-    choices.forEach(function(choice){
-        var button = document.createElement("button");
-        button.textContent = choice;
-        button.addEventListener("click", checkAnswer)
-        qBox.appendChild(button);
-      })
-      
-}
-// create a function that will check the answr to see if it is correct or incorrect
-function checkAnswer(event){
-    //console.log(event.target.textContent)
-    if(questions[index].answer === event.target.textContent){
-        score++;
-        alert("Correct!")
-    var scoreBox= document.getElementById("scoreBox");
-    console.log 
-    scoreBox.innerHTML= "Score: " + score;
-        
     } else {
-        //aka:secondsLeft = secondsLeft - 15;
-        secondsLeft -= 15;
-        alert("Wrong. -15 seconds")
+          
+        var qBox = document.querySelector("#questionBox");
+        qBox.textContent = "";
+        var title = questions[index].title;
+
+        var titleEl = document.createElement("h2");
+        titleEl.textContent = title;
+        qBox.appendChild(titleEl);
+
+        var choices = questions[index].choices;
+        choices.forEach(function(choice){
+            var button = document.createElement("button");
+            button.textContent = choice;
+            button.addEventListener("click", checkAnswer)
+            qBox.appendChild(button);
+        })
+
     }
-    index++
-    //if else check if next question is there, else 
-    // function gameover displaying score, stop timer
+}
+// create a function that will check the answer to see if it is correct or incorrect
+function checkAnswer(event){
+    var answerBox= document.getElementById("answerBox");
+    if(questions[index].answer === event.target.textContent){
+        answerBox.innerHTML= "Correct"; 
+        
+// If the question is wrong, implement a -2 second penalty
+    } else {
+        //aka:secondsLeft = secondsLeft - 2;
+        secondsLeft -= 2;
+        answerBox.innerHTML= "Incorrect";
+    }
+        
     displayQuestion()
 }
 // TODO: function storeScores() {
@@ -81,10 +86,10 @@ function checkAnswer(event){
 //I DONT THINK THIS WORKS..
 document.getElementById("start").addEventListener("click", gameStart)
 
-//Allow user to "View Highscore" & "Time" value set at 0
-// Make a timer that starts at 75 minutes and goes down to 0 
-// For correct answers increase the score by 1
-// For any incorrect answers notify player and subtract 15 seconds 
-// At the end of the game, display final score 
-// prompt user to input their initials 
-// store the scores and initials in localStorage 
+//TODO:Allow user to "View Highscore" & "Time" value set at 0
+
+function displayGameOver() {
+    alert ("Game Over");
+    clearInterval(timerInterval);
+    score = secondsLeft;
+}
--- questions.js
+++ questions.js
@@ -9,4 +9,14 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
+    {
+      title: "What color is the sky?",
+      choices: ["blue", "green", "purple", "pink"], 
+      answer: "blue"
+    },
+    {
+      title: "How old am I?",
+      choices: ["20", "22", "24", "26" ], 
+      answer: "24"
+    }
   ]; 
